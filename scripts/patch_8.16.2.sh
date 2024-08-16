064eca7edd980440b510f70f88f3e2d16ffe5010
nicolaslopezneotelus <nicolas.lopez@neotel.us>
2024-08-16 15:15:20 -0300

    PBX Script Installer for Rocky Linux El script del parche 2 ha sido borrado y se ha renombrado el 3 y el 4.

--- scripts/patch_8.16.2.sh
+++ scripts/patch_8.16.2.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
 SOURCEDIR=/usr/src
-RELEASE=`rpm -q rocky-release`
 
-function exec {
-  "$@"
-  local EXIT_CODE=$?
-  if [ $EXIT_CODE -ne 0 ]; then
-    echo "$* failed with exit code $EXIT_CODE." >&2
-    exit
-  fi
-}
+#Apply asterisk database patches
+echo "Applying asterisk database patches..."
+cd $SOURCEDIR/PBX-1.8-RL94/patches/8.16.3/asterisk/database/
+dos2unix database.sh
+su postgres ./database.sh
 
-clear
+cp -f /etc/asterisk/sip.conf /etc/asterisk/sip.conf.bak
+sed -e "s/#tcpblindaddr=0.0.0.0:5060//" /etc/asterisk/sip.conf > /etc/asterisk/sip.conf.tmp && mv -f /etc/asterisk/sip.conf.tmp /etc/asterisk/sip.conf
+echo "#tcpblindaddr=0.0.0.0:5060" >> /etc/asterisk/sip.conf
+sed -e "s/#tcpenabled=yes//" /etc/asterisk/sip.conf > /etc/asterisk/sip.conf.tmp && mv -f /etc/asterisk/sip.conf.tmp /etc/asterisk/sip.conf
+echo "#tcpenabled=yes" >> /etc/asterisk/sip.conf
 
-#Modify the file permissions
-chmod -R 755 $SOURCEDIR/*
+#Copy configuration files
+echo "Copying configuration files..."
+cp -fr $SOURCEDIR/PBX-1.8-RL94/patches/8.16.3/scripts/* /etc/asterisk/
 
-if [[ $RELEASE = "rocky-release-5.14.0-427.13.1.el9_4.x86_64.noarch" ]]; then
-  cd $SOURCEDIR/PBX-1.8-RL94/packages/x86_64/5.14.0-427.13.1.el9_4.x86_64/
-
-  #Install software dependencies
-  echo "Installing software dependencies..."
-  #yum install lm_sensors
-  exec rpm -Uvh --force lm_sensors-3.4.0-23.20180522git70f7e08.el8.x86_64.rpm 
-else
-  echo "Release $RELEASE not supported"
-  exit
+#Install watcher
+echo "Installing watcher..."
+cd $SOURCEDIR/PBX-1.8-RL94/patches/8.16.3/watcher/src/
+gcc -D HAVE_INOTIFY -I/usr/include/inotifytools -I/usr/include/curl -O0 -g3 -Wall -lpthread -linotifytools -lcurl watcher.c list.c logger.c thpool.c -o watcher
+chmod 755 watcher
+service watcher stop
+cp -fr watcher /usr/sbin/
+cd $SOURCEDIR/PBX-1.8-RL94/patches/8.16.3/watcher/
+dos2unix watcher
+chmod 755 watcher
+cp -fr watcher /etc/init.d/
+if [ ! -d /etc/watcher ]; then
+  mkdir /etc/watcher
 fi
-
-#Configure sensors
-echo "Configuring sensors..."
-YES "" | sensors-detect
-
-#Configure scheduled tasks
-echo "Configuring scheduled tasks..."
-cp -f $SOURCEDIR/PBX-1.8-RL94/patches/8.16.1/cron/root /var/spool/cron/root
-chmod 644 /var/spool/cron/root
-service crond restart
+dos2unix watcher.conf
+cp -fr watcher.conf /etc/watcher/
+systemctl enable watcher #chkconfig watcher on
+service watcher restart
--- scripts/patch_8.16.3.sh
+++ scripts/patch_8.16.3.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 SOURCEDIR=/usr/src
+RELEASE=`rpm -q rocky-release`
 
-#Apply asterisk database patches
-echo "Applying asterisk database patches..."
-cd $SOURCEDIR/PBX-1.8-RL94/patches/8.16.3/asterisk/database/
-dos2unix database.sh
-su postgres ./database.sh
+function exec {
+  "$@"
+  local EXIT_CODE=$?
+  if [ $EXIT_CODE -ne 0 ]; then
+    echo "$* failed with exit code $EXIT_CODE." >&2
+    exit
+  fi
+}
 
-cp -f /etc/asterisk/sip.conf /etc/asterisk/sip.conf.bak
-sed -e "s/#tcpblindaddr=0.0.0.0:5060//" /etc/asterisk/sip.conf > /etc/asterisk/sip.conf.tmp && mv -f /etc/asterisk/sip.conf.tmp /etc/asterisk/sip.conf
-echo "#tcpblindaddr=0.0.0.0:5060" >> /etc/asterisk/sip.conf
-sed -e "s/#tcpenabled=yes//" /etc/asterisk/sip.conf > /etc/asterisk/sip.conf.tmp && mv -f /etc/asterisk/sip.conf.tmp /etc/asterisk/sip.conf
-echo "#tcpenabled=yes" >> /etc/asterisk/sip.conf
+clear
 
-#Copy configuration files
-echo "Copying configuration files..."
-cp -fr $SOURCEDIR/PBX-1.8-RL94/patches/8.16.3/scripts/* /etc/asterisk/
+#Modify the file permissions
+chmod -R 755 $SOURCEDIR/*
 
-#Install watcher
-echo "Installing watcher..."
-cd $SOURCEDIR/PBX-1.8-RL94/patches/8.16.3/watcher/src/
-gcc -D HAVE_INOTIFY -I/usr/include/inotifytools -I/usr/include/curl -O0 -g3 -Wall -lpthread -linotifytools -lcurl watcher.c list.c logger.c thpool.c -o watcher
-chmod 755 watcher
-service watcher stop
-cp -fr watcher /usr/sbin/
-cd $SOURCEDIR/PBX-1.8-RL94/patches/8.16.3/watcher/
-dos2unix watcher
-chmod 755 watcher
-cp -fr watcher /etc/init.d/
-if [ ! -d /etc/watcher ]; then
-  mkdir /etc/watcher
+if [[ $RELEASE = "rocky-release-9.4-1.5.el9.noarch" ]]; then
+  cd $SOURCEDIR/PBX-1.8-RL94/packages/x86_64/5.14.0-427.13.1.el9_4.x86_64
+
+  #yum install hyperv-daemons
+  exec rpm -Uvh --force hypervkvpd-0-0.42.20190303git.el9.x86_64.rpm \
+                        hypervfcopyd-0-0.42.20190303git.el9.x86_64.rpm \
+                        hypervvssd-0-0.42.20190303git.el9.x86_64.rpm   \
+                        hyperv-daemons-0-0.42.20190303git.el9.x86_64.rpm \
+                        hyperv-daemons-license-0-0.42.20190303git.el9.noarch.rpm
+
+  #Copy configuration files
+  echo "Copying configuration files..."
+  cp -fr $SOURCEDIR/PBX-1.8-RL94/patches/8.16.4/asterisk/configs/* /etc/asterisk/
+  dos2unix /etc/asterisk/*
+  echo
+else
+  echo "Release $RELEASE not supported"
+  exit
 fi
-dos2unix watcher.conf
-cp -fr watcher.conf /etc/watcher/
-systemctl enable watcher #chkconfig watcher on
-service watcher restart
+
+#reboot
